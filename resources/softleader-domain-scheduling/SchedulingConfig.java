f3089497c5e493074fb436bfad59db036da1cd95
Matt S.Y. Ho <methodho@gmail.com>
2016-07-19 18:11:00 +0800

    update

--- resources/softleader-domain-scheduling/SchedulingConfig.java
+++ resources/softleader-domain-scheduling/SchedulingConfig.java
@@ -1,31 +1,28 @@
 package {pkg}.config;
 
-import org.quartz.spi.JobFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 import tw.com.softleader.domain.scheduling.LeaderElectionTaskScheduler;
 import tw.com.softleader.domain.scheduling.ScheduleManager;
 import tw.com.softleader.domain.scheduling.ScheduleManagers;
 import tw.com.softleader.domain.scheduling.leader.CuratorLeaderLatch;
 import tw.com.softleader.domain.scheduling.leader.LeaderElection;
-import tw.com.softleader.domain.scheduling.quartz.AutowiringSpringBeanJobFactory;
-import tw.com.softleader.domain.scheduling.quartz.LeaderElectionJobFactory;
 
 @Configuration
 @EnableAsync
 @EnableScheduling
 @ComponentScan(basePackages = "{pkg}.job")
 @PropertySource({"classpath:schedule.properties"})
-public class I519SchedulingConfig {
+public class SchedulingConfig {
 
   @Value("${zookeeper.connect.string}")
   private String connectString;
